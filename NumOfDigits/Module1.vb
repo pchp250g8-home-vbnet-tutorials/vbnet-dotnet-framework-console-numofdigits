58aa836b313921806e7c2f031e0686b4bc8c12ee
pchp250g8-home <pchp250g8@tutamail.com>
2025-12-03 19:21:59 +0300

    NumOfDigits

---
+++ NumOfDigits/Module1.vb
@@ -0,0 +1,17 @@
+﻿Module Module1
+
+    Sub Main()
+        Console.WriteLine("Input an Integer Number:")
+        Dim strNum = Console.ReadLine()
+        Dim iNum As Long = 0
+        Int64.TryParse(strNum, iNum)
+        Dim nDigits = 0 : Dim iVal = Math.Abs(iNum)
+        Do While (iVal > 0)
+            iVal \= 10
+            nDigits += 1
+        Loop
+        Console.WriteLine("The number {0} has {1} digit(s)", iNum, nDigits)
+        Console.Read()
+    End Sub
+
+End Module
---
+++ NumOfDigits/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Общие сведения об этой сборке предоставляются следующим набором
+' набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+' связанные со сборкой.
+
+' Проверьте значения атрибутов сборки
+
+<Assembly: AssemblyTitle("NumOfDigits")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("NumOfDigits")>
+<Assembly: AssemblyCopyright("Copyright ©  2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+<Assembly: Guid("1c660aab-9034-4618-b010-48a76beae375")>
+
+' Сведения о версии сборки состоят из следующих четырех значений:
+'
+'      Основной номер версии
+'      Дополнительный номер версии
+'   Номер сборки
+'      Редакция
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
